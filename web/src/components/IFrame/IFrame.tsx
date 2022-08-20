1ab0f72956737426f8ff196098ece5f5d11f49e2
Will  Kelly <willkelly0@gmail.com>
2022-08-20 13:40:29 -0400

    next

--- web/src/components/IFrame/IFrame.tsx
+++ web/src/components/IFrame/IFrame.tsx
@@ -5,6 +5,8 @@ interface IFrameProps {
   src: string;
 }
 
+const HTMLSTRING = `<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Regular reminder: all 5000+ of my instagram accounts are fake profiles and have nothing to do with me. Anything you see &quot;Vitalik Buterin&quot; (or &quot;Bütеirn&quot; or whatever) posting on instagram is a scam.</p>&mdash; vitalik.eth (@VitalikButerin) <a href="https://twitter.com/VitalikButerin/status/1558893139959709697?ref_src=twsrc%5Etfw">August 14, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>";`;
+
 export function IFrame({ src }: IFrameProps) {
   console.log(html);
   var parser = new DOMParser();
@@ -28,7 +30,7 @@ export function IFrame({ src }: IFrameProps) {
         }}
       >
         <iframe
-          srcDoc={`${htmlDoc}`}
+          srcDoc={HTMLSTRING}
           width="80%"
           height="100%"
           scrolling="no"
