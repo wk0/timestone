4c1fbdbbb5b1d756bb6988e2e6b533551fe2e8a6
Benjamin Che <ben@triple.finance>
2022-08-19 21:59:07 -0500

    material-ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { ConnectButton } from "./components";
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>
       <h1>Timestone</h1>
       <ConnectButton />
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
