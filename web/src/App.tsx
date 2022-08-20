f852a3eac2ce7c84700f75c451c13b9bf161b587
Will  Kelly <willkelly0@gmail.com>
2022-08-19 23:13:32 -0400

    fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "React";
+import { useState } from "react";
 import { ConnectButton, IFrame } from "./components";
 import Button from "@mui/material/Button";
 
