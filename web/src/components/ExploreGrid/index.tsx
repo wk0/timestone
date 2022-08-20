9bceb8cdd62f41ee486d2ac9624b4a2e90c617c9
Benjamin Che <ben@triple.finance>
2022-08-20 12:00:55 -0500

    grid cards

---
+++ web/src/components/ExploreGrid/index.tsx
@@ -0,0 +1,48 @@
+import {
+  Box,
+  Grid,
+  Typography,
+} from '@mui/material';
+import GridCard from '../GridCard';
+
+const ExploreGrid = (props:any) => {
+  
+  const { timestones } = props;
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          minHeight: '100%',
+          width: '100%',
+          marginTop: '1.5rem'
+        }}
+      >
+        <Grid
+          container
+          spacing={3}
+        >
+          {timestones && timestones.map((stone:any) => (
+            <GridCard stone={stone} />
+          ))}
+          {(!timestones || timestones.length <= 0) && (
+            <Grid
+              item
+              lg={3}
+              md={4}
+              xs={12}
+            >
+              <Typography variant="body1">
+                Nothing here yet!
+              </Typography>
+            </Grid>
+          )}
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default ExploreGrid;
