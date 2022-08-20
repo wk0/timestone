a2d365436fdb4d3fdd98ef8559963394040404d7
Benjamin Che <ben@triple.finance>
2022-08-19 22:12:31 -0500

    theming

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,27 @@
 import "./App.css";
 import { ConnectButton } from "./components";
 import Button from '@mui/material/Button';
+import { createCustomTheme } from './theme';
+import { ThemeProvider } from '@mui/material/styles';
 
-function App() {
+const App = () => {
+
+  const theme = createCustomTheme({
+    direction: 'ltr',
+    compact: true,
+    roundedCorners: true,
+    theme: 'DARK'
+  });
+
   return (
-    <div>
-      <h1>Timestone</h1>
-      <ConnectButton />
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>Timestone</h1>
+        <ConnectButton />
+        <Button variant="contained">Hello World</Button>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ web/src/theme/index.ts
@@ -0,0 +1,214 @@
+import merge from 'lodash/merge';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { THEMES } from './constants';
+import { lightShadows } from './shadows';
+
+const baseOptions = {
+  direction: 'ltr',
+  components: {
+    MuiAvatar: {
+      styleOverrides: {
+        fallback: {
+          height: '75%',
+          width: '75%'
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          borderRadius: '6px'
+        }
+      }
+    },
+    MuiCssBaseline: {
+      styleOverrides: {
+        '*': {
+          boxSizing: 'border-box'
+        },
+        html: {
+          MozOsxFontSmoothing: 'grayscale',
+          WebkitFontSmoothing: 'antialiased',
+          height: '100%',
+          width: '100%'
+        },
+        body: {
+          height: '100%'
+        },
+        '#root': {
+          height: '100%'
+        },
+        '#nprogress .bar': {
+          zIndex: '2000 !important'
+        }
+      }
+    },
+    MuiCardHeader: {
+      defaultProps: {
+        titleTypographyProps: {
+          variant: 'h6'
+        }
+      }
+    },
+    MuiBackdrop: {
+      styleOverrides: {
+        root: {
+          backgroundColor: 'rgba(0, 0, 0, 0.75)'
+        }
+      }
+    },
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          color: "#FFF",
+          textAlign: "center",
+          fontSize: "15px",
+          lineHeight: "21px",
+          backgroundColor: "#1C1E1D",
+          padding: "8px 16px",
+          boxShadow: "0px 8px 16px rgba(31, 34, 52, 0.6)",
+          borderRadius: "16px",
+        },
+      },
+    },
+    MuiLinearProgress: {
+      styleOverrides: {
+        root: {
+          borderRadius: 3,
+          overflow: 'hidden'
+        }
+      }
+    },
+    MuiListItemIcon: {
+      styleOverrides: {
+        root: {
+          minWidth: 'auto',
+          marginRight: '16px'
+        }
+      }
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundImage: 'none'
+        }
+      }
+    }
+  },
+  typography: {
+    button: {
+      fontWeight: 600
+    },
+    fontFamily: '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+    h1: {
+      fontWeight: 600,
+      fontFamily: 'Roboto Mono',
+      fontSize: '3.5rem'
+    },
+    h2: {
+      fontWeight: 600,
+      fontFamily: 'Roboto Mono',
+      fontSize: '3rem'
+    },
+    h3: {
+      fontWeight: 600,
+      fontFamily: 'Roboto Mono',
+      fontSize: '2.25rem'
+    },
+    h4: {
+      fontWeight: 600,
+      fontFamily: 'Roboto Mono',
+      fontSize: '2rem'
+    },
+    h5: {
+      fontWeight: 600,
+      fontFamily: 'Roboto Mono',
+      fontSize: '1.5rem'
+    },
+    h6: {
+      fontWeight: 600,
+      fontFamily: 'Roboto Mono',
+      fontSize: '1.125rem'
+    },
+    overline: {
+      fontWeight: 600,
+      fontSize: '1rem'
+    },
+    subtitle: {
+      fontWeight: 400,
+      color: '#b4bac6',
+      fontSize: '1rem'
+    }
+  }
+};
+
+const themesOptions = {
+  [THEMES.LIGHT]: {
+    components: {
+      MuiInputBase: {
+        styleOverrides: {
+          input: {
+            '&::placeholder': {
+              opacity: 0.86,
+              color: '#42526e'
+            }
+          }
+        }
+      }
+    },
+    palette: {
+      action: {
+        active: '#6b778c'
+      },
+      background: {
+        default: '#f4f5f7',
+        paper: '#ffffff'
+      },
+      error: {
+        contrastText: '#ffffff',
+        main: '#f44336'
+      },
+      mode: 'light',
+      primary: {
+        contrastText: '#ffffff',
+        main: '#5664d2'
+      },
+      success: {
+        contrastText: '#ffffff',
+        main: '#4caf50'
+      },
+      text: {
+        primary: '#172b4d',
+        secondary: '#6b778c'
+      },
+      warning: {
+        contrastText: '#ffffff',
+        main: '#ff9800'
+      }
+    },
+    shadows: lightShadows
+  }
+};
+
+export const createCustomTheme = (config:any) => {
+  
+  let themeOptions = themesOptions[config.theme];
+
+  if (!themeOptions) {
+    console.warn(new Error(`The theme ${config.theme} is not valid`));
+    themeOptions = themesOptions[THEMES.LIGHT];
+  }
+
+  let theme = createTheme(merge({}, baseOptions, themeOptions, {
+    ...(config.roundedCorners && {
+      shape: {
+        borderRadius: 16
+      }
+    })
+  }, {
+    direction: config.direction
+  }));
+
+  return theme;
+};
