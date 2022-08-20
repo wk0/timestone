59d56fcaf6900777d6be07af554e7226b55973e6
Benjamin Che <ben@triple.finance>
2022-08-20 00:48:26 -0500

    rework theme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,20 +2,12 @@ import "./App.css";
 import { ConnectButton } from "./components";
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { createCustomTheme } from './theme';
-import { ThemeProvider } from '@mui/material/styles';
+import ThemeProvider from './theme/ThemeProvider';
 
 const App = () => {
 
-  const theme = createCustomTheme({
-    direction: 'ltr',
-    compact: true,
-    roundedCorners: true,
-    theme: 'DARK'
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider>
       <div>
         <Typography variant="h1">Timestone</Typography>
         <ConnectButton />
