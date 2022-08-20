1b1ea4a3d4083f956c6e7143d55b835851638f3f
Will  Kelly <willkelly0@gmail.com>
2022-08-19 23:04:28 -0400

    iframe

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
-import { ConnectButton } from "./components";
+import { useState } from "React";
+import { ConnectButton, IFrame } from "./components";
 
 function App() {
+  const [url, setURL] = useState("https://example.com");
+
   return (
     <div>
       <h1>Timestone</h1>
       <ConnectButton />
+      <IFrame src={url} />
     </div>
   );
 }
