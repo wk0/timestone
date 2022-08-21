e0b751f0647057301b5a447b5f3f13c8fb6a9b2d
Will  Kelly <willkelly0@gmail.com>
2022-08-21 01:49:20 -0400

    call fetch in input box, pass snapshot to component

--- next/components/InputBox/index.tsx
+++ next/components/InputBox/index.tsx
@@ -1,21 +1,37 @@
-import { Box, Container, Typography, Grid, useTheme, useMediaQuery, Paper, InputBase } from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
-import React, { useEffect, useState } from 'react';
-import FilterTiltShiftIcon from '@mui/icons-material/FilterTiltShift';
-import Mint from '../Mint';
+import {
+  Box,
+  Container,
+  Typography,
+  Grid,
+  useTheme,
+  useMediaQuery,
+  Paper,
+  InputBase,
+} from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import React, { useEffect, useState } from "react";
+import FilterTiltShiftIcon from "@mui/icons-material/FilterTiltShift";
+import Mint from "../Mint";
 
-// Custom themed from Material-Ui InputBase component 
+// Custom themed from Material-Ui InputBase component
 const CustomizedInputBase = (props: any) => {
-
   const { placeholder, value, onChange, onKeyPress } = props;
 
   return (
     <Paper
       component="form"
-      sx={{ p: '0px 16px', display: 'flex', alignItems: 'center', width: 500, height: 75, borderRadius: 0, fontSize: '30px!important' }}
+      sx={{
+        p: "0px 16px",
+        display: "flex",
+        alignItems: "center",
+        width: 500,
+        height: 75,
+        borderRadius: 0,
+        fontSize: "30px!important",
+      }}
     >
       <InputBase
-        sx={{ ml: 2, flex: 1, mt: '0px' }}
+        sx={{ ml: 2, flex: 1, mt: "0px" }}
         placeholder={placeholder}
         inputProps={{ style: { fontSize: 25 } }}
         value={value}
@@ -28,9 +44,8 @@ const CustomizedInputBase = (props: any) => {
 };
 
 const InputBox = ({ prepopulate }: any) => {
-
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const [urlInput, setUrlInput] = useState("");
   const handleUrlInputChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,12 +56,30 @@ const InputBox = ({ prepopulate }: any) => {
   const [showMint, setShowMint] = useState(false);
 
   const [isSnapshotting, setIsSnapshotting] = useState(false);
+  const [snapshotURI, setSnapshotURI] = useState<string | null>(null);
+
   const triggerSnapshot = () => {
     setShowMint(true);
     setIsSnapshotting(true);
-    setTimeout(() => {
-      setIsSnapshotting(false);
-    }, 2000);
+
+    fetch("/api/snapshot", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: urlInput }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        const snapshotImage = data.snapshot;
+        setSnapshotURI(snapshotImage);
+        setIsSnapshotting(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setIsSnapshotting(false);
+      });
   };
 
   const onKeyPress = (ev: React.KeyboardEvent<HTMLInputElement>) => {
@@ -60,37 +93,52 @@ const InputBox = ({ prepopulate }: any) => {
   return (
     <Box
       sx={{
-        backgroundColor: 'background.paper',
-        pt: 2
+        backgroundColor: "background.paper",
+        pt: 2,
       }}
     >
       <Container
         maxWidth="lg"
         sx={{
-          alignItems: 'flex-start',
-          display: 'flex',
-          flexDirection: 'column',
+          alignItems: "flex-start",
+          display: "flex",
+          flexDirection: "column",
           px: {
-            md: '130px !important',
-            xs: 4
-          }
+            md: "130px !important",
+            xs: 4,
+          },
         }}
       >
         <Grid container justifyContent="center">
-          <CustomizedInputBase value={urlInput} onChange={handleUrlInputChange} onKeyPress={onKeyPress} placeholder={"nytimes.com"} />
+          <CustomizedInputBase
+            value={urlInput}
+            onChange={handleUrlInputChange}
+            onKeyPress={onKeyPress}
+            placeholder={"nytimes.com"}
+          />
           <LoadingButton
             disabled={!urlInput}
             loading={isSnapshotting}
             variant="text"
             onClick={triggerSnapshot}
-            style={{ marginLeft: '1.1rem', borderRadius: 0, paddingLeft: '8px', paddingRight: '8px', width: '75px' }}
+            style={{
+              marginLeft: "1.1rem",
+              borderRadius: 0,
+              paddingLeft: "8px",
+              paddingRight: "8px",
+              width: "75px",
+            }}
           >
-            <FilterTiltShiftIcon style={{ fontSize: '38px' }} />
+            <FilterTiltShiftIcon style={{ fontSize: "38px" }} />
           </LoadingButton>
         </Grid>
       </Container>
       {showMint && (
-        <Mint urlInput={urlInput} isSnapshotting={isSnapshotting} />
+        <Mint
+          urlInput={urlInput}
+          isSnapshotting={isSnapshotting}
+          snapshotURI={snapshotURI}
+        />
       )}
     </Box>
   );
